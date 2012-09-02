2f3c7c1baa837121c4fe374be5d0767ee6f5069d
David Alpert <david@spinthemoose.com>
2012-09-01 19:47:43 -0500

    importing v1.0.2 (hg changeset 8e1d4d6874e1 at http://powerassert.codeplex.com/SourceControl/changeset/8e1d4d6874e1)

---
+++ PowerAssert/Infrastructure/Util.Generated.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using System.Collections.Generic;
+
+namespace PowerAssert.Infrastructure
+{
+    internal class Util
+    {
+        internal static Dictionary<ExpressionType, string> Operators = new Dictionary<ExpressionType, string>
+        {
+            {ExpressionType.AndAlso, "&&"},
+            {ExpressionType.OrElse, "||"},
+            {ExpressionType.Add, "+"},
+            {ExpressionType.AddChecked, "+"},
+            {ExpressionType.And, "&"},
+            {ExpressionType.Divide, "/"},
+            {ExpressionType.Equal, "=="},
+            {ExpressionType.ExclusiveOr, "^"},
+            {ExpressionType.GreaterThan, ">"},
+            {ExpressionType.GreaterThanOrEqual, ">="},
+            {ExpressionType.LeftShift, "<<"},
+            {ExpressionType.LessThan, "<"},
+            {ExpressionType.LessThanOrEqual, "<="},
+            {ExpressionType.Modulo, "%"},
+            {ExpressionType.Multiply, "*"},
+            {ExpressionType.MultiplyChecked, "*"},
+            {ExpressionType.NotEqual, "!="},
+            {ExpressionType.Or, "|"},
+            {ExpressionType.RightShift, ">>"},
+            {ExpressionType.Subtract, "-"},
+            {ExpressionType.SubtractChecked, "-"},
+        };
+    }
+}
---
+++ PowerAssert/PAssert.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using PowerAssert.Infrastructure;
+using PowerAssert.Infrastructure.Nodes;
+
+namespace PowerAssert
+{
+    public static class PAssert
+    {
+        public static TException Throws<TException>(Action a) where TException : Exception
+        {
+            try
+            {
+                a();
+            }
+            catch(TException exception)
+            {
+                return exception;
+            }
+
+            throw new Exception("An exception of type " + typeof(TException).Name + " was expected, but no exception occured");
+        }
+
+        public static void IsTrue(Expression<Func<bool>> expression)
+        {
+            Func<bool> func = expression.Compile();
+            if (!func())
+            {
+                throw CreateException(expression, "IsTrue failed");
+            }
+        }
+
+        static Exception CreateException(Expression<Func<bool>> expression, string message)
+        {
+            Node constantNode = ExpressionParser.Parse(expression.Body);
+            string[] lines = NodeFormatter.Format(constantNode);
+            string nl = Environment.NewLine;
+            return new Exception(message + ", expression was:" + nl + nl + String.Join(nl, lines));
+        }
+    }
+}
