1d9b460bd64e3d600453792cb1da949b5fcd8621
Rob Fonseca-Ensor <robert.ensor@gmail.com>
2013-03-08 11:32:43 +1300

    cleaned up R# warnings in InvocationNode.cs

--- PowerAssert/Infrastructure/Nodes/InvocationNode.cs
+++ PowerAssert/Infrastructure/Nodes/InvocationNode.cs
@@ -1,28 +1,26 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
-using PowerAssert.Infrastructure.Nodes;
 
 namespace PowerAssert.Infrastructure.Nodes
 {
     internal class InvocationNode : Node
     {
-        [CanBeNull]
+        [NotNull]
         public IEnumerable<Node> Arguments { get; set; }
 
         [NotNull]
         public Node Expression { get; set; }
 
-        internal override void Walk(Node.NodeWalker walker, int depth)
+        internal override void Walk(NodeWalker walker, int depth)
         {
-            this.Expression.Walk(walker, depth);
+            Expression.Walk(walker, depth);
             walker("(");
-            foreach (var arg in this.Arguments.Take(1))
+            foreach (var arg in Arguments.Take(1))
             {
                 arg.Walk(walker, depth);
             }
-            foreach (var arg in this.Arguments.Skip(1))
+            foreach (var arg in Arguments.Skip(1))
             {
                 walker(", ");
                 arg.Walk(walker, depth);
