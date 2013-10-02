3c81ad9d641da698b96b41a3f27b5986eb41136f
George Pollard <porges@porg.es>
2013-10-02 22:44:51 +1300

    Tests for "Broken Equality" hint

--- PowerAssert/Hints/BrokenEqualityHint.cs
+++ PowerAssert/Hints/BrokenEqualityHint.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using PowerAssert.Infrastructure;
@@ -11,24 +12,50 @@ namespace PowerAssert.Hints
 
         public bool TryGetHint(Expression expression, out string hint)
         {
+            object left = null;
+            object right = null;
+            Type declaringType = null;
+
             var methE = expression as MethodCallExpression;
             if (methE != null)
             {
                 if (methE.Method == ObjectEqualsMethodInfo)
                 {
-                    var left = ExpressionParser.DynamicInvoke(methE.Object);
-                    var right = ExpressionParser.DynamicInvoke(methE.Arguments[0]);
+                    left = ExpressionParser.DynamicInvoke(methE.Object);
+                    right = ExpressionParser.DynamicInvoke(methE.Arguments[0]);
 
-                    if (left != null && left == right)
-                    {
-                        hint = ", type " + ExpressionParser.NameOfType(left.GetType()) + " has a broken equality implementation (both sides are the same object)";
-                        return true;
-                    }
+                    // methE.Method.DeclaringType doesn't work here, it will
+                    // always return the base class method
+                    declaringType = GetDerivedMethodInfo(methE.Object.Type, methE.Method).DeclaringType;
                 }
+            }
+
+            var binE = expression as BinaryExpression;
+            if (binE != null && binE.NodeType == ExpressionType.Equal)
+            {
+                left = ExpressionParser.DynamicInvoke(binE.Left);
+                right = ExpressionParser.DynamicInvoke(binE.Right);
+
+                if (binE.Method != null)
+                    declaringType = binE.Method.DeclaringType;
+                else declaringType = typeof (object); // this should never happen ;)
+            }
+
+            if (left != null && left == right)
+            {
+                hint = ", type " + ExpressionParser.NameOfType(declaringType) + " has a broken equality implementation (both sides are the same object)";
+                return true;
             }
 
             hint = null;
             return false;
+        }
+
+        // this can be improved
+        private static MethodInfo GetDerivedMethodInfo(Type derivedType, MethodInfo baseMethod)
+        {
+            return derivedType.GetMethod(baseMethod.Name,
+                baseMethod.GetParameters().Select(x => x.ParameterType).ToArray());
         }
     }
 }
---
+++ PowerAssertTests/Hints/BrokenEqualityHintTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using NUnit.Framework;
+using PowerAssert.Hints;
+
+namespace PowerAssertTests.Hints
+{
+    [TestFixture]
+    public class BrokenEqualityHintTests
+    {
+        [Test]
+        public void DoesntTriggerWithDifferingObjectsForMethodEquals()
+        {
+            var hint = new BrokenEqualityHint();
+
+            var x = new object();
+            var y = new object();
+
+            Expression<Func<bool>> exp = () => x.Equals(y);
+
+            string ignored;
+            Assert.IsFalse(hint.TryGetHint(exp.Body, out ignored));
+
+            Assert.IsNull(ignored);
+        }
+
+        [Test]
+        public void TriggersOnSameObjectForMethodEquals()
+        {
+            var hint = new BrokenEqualityHint();
+
+            var x = new object();
+
+            Expression<Func<bool>> exp = () => x.Equals(x);
+
+            string ignored;
+            Assert.IsTrue(hint.TryGetHint(exp.Body, out ignored));
+            Assert.IsNotNull(ignored);
+        }
+        
+
+        [Test]
+        public void TriggersOnSameObjectForOperatorEquals()
+        {
+            var hint = new BrokenEqualityHint();
+
+            var x = new object();
+
+            Expression<Func<bool>> exp = () => x == x;
+
+            string ignored;
+            Assert.IsTrue(hint.TryGetHint(exp.Body, out ignored));
+
+            Assert.IsNotNull(ignored);
+        }
+
+        [Test]
+        public void DoesntTriggerWithDifferingObjectsForOperatorEquals()
+        {
+            var hint = new BrokenEqualityHint();
+
+            var x = new object();
+            var y = new object();
+
+            Expression<Func<bool>> exp = () => x == y;
+
+            string ignored;
+            Assert.IsFalse(hint.TryGetHint(exp.Body, out ignored));
+
+            Assert.IsNull(ignored);
+        }
+
+    
+        [Test]
+        public void PicksUpCorrectTypeForOverriddenMethodEquals()
+        {
+            var hint = new BrokenEqualityHint();
+
+            var x = new OverridesEqualsBadly();
+
+            Expression<Func<bool>> exp = () => x.Equals(x);
+
+            string ignored;
+            Assert.IsTrue(hint.TryGetHint(exp.Body, out ignored));
+
+            Assert.IsNotNull(ignored);
+            Assert.IsTrue(ignored.Contains(x.GetType().Name));
+        }
+
+        [Test]
+        public void PicksUpCorrectTypeForOverriddenOperatorEquals()
+        {
+            var hint = new BrokenEqualityHint();
+
+            var x = new OverridesEqualsBadly();
+
+            Expression<Func<bool>> exp = () => x == x;
+
+            string ignored;
+            Assert.IsTrue(hint.TryGetHint(exp.Body, out ignored));
+
+            Assert.IsNotNull(ignored);
+            Assert.IsTrue(ignored.Contains(x.GetType().Name));
+        }
+
+        class OverridesEqualsBadly
+        {
+            public override bool Equals(object obj)
+            {
+                return false;
+            }
+
+            public static bool operator ==(OverridesEqualsBadly l, OverridesEqualsBadly r)
+            {
+                return false;
+            }
+
+            public static bool operator !=(OverridesEqualsBadly l, OverridesEqualsBadly r)
+            {
+                return true;
+            }
+        }
+    }
+}
