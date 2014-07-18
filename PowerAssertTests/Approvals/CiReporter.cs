7785575d4c0f66a29249c8369dad6ef297a19b66
Robert Fonseca-Ensor <rfe@element26software.com>
2014-07-19 11:07:00 +1200

    CI reported prints differences

--- PowerAssertTests/Approvals/CiReporter.cs
+++ PowerAssertTests/Approvals/CiReporter.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.IO;
+using ApprovalTests.Core;
 using ApprovalTests.Reporters;
 
 namespace PowerAssertTests.Approvals
 {
-    public class CiReporter : QuietReporter
+    public class CiReporter : IEnvironmentAwareReporter, IApprovalFailureReporter
     {
-        public override bool IsWorkingInThisEnvironment(string forFile)
+        public bool IsWorkingInThisEnvironment(string forFile)
         {
             return Environment.GetEnvironmentVariable("CI_SERVER") != null;
+        }
+
+        public void Report(string approved, string received)
+        {
+            Console.Out.WriteLine("APPROVED:");
+            Console.Out.WriteLine(File.ReadAllText(approved));
+            Console.Out.WriteLine();
+            Console.Out.WriteLine("RECEIVED:");
+            Console.Out.WriteLine(File.ReadAllText(received));
         }
     }
 }
